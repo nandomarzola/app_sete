69cce0cbc384c57fa3d63ba036c9ca68713bf2e3
Nando Marzola <nandomarzola1@gmail.com>
2021-03-03 14:45:06 -0300

    finsh dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,15 +1,35 @@
 import React, { useState } from 'react';
 
+import { Picker} from '@react-native-community/picker';
+
 import {
    Container,
+   DashboardArea,
    HeaderDashboard,
    HeaderTitle,
-   HeaderSelectDay,
-   AreaSelect
+   AreaIcon,
+   AreaDays,
+   AreaSelect,
+   Scroller,
+   ScrollItem,
+   ScrollerItens,
+   Day,
+   Month,
+   LoginsAndErrorsArea,
+   Logins,
+   Errors,
+   Title,
+   Total,
+   VisitorsArea,
+   ListVisits,
+   IpVisits,
+   TotalVisits
   } from './styles';
 
   import Ads from '../../components/Ads';
   import Header from '../../components/Header';
+
+  import IconDrop from '../../assets/icons/expand.svg';
 
 export default() => {
 
@@ -18,31 +38,123 @@ export default() => {
 return(
   <Container>
          <Header />
-         <HeaderDashboard>
-            <HeaderTitle>Dashboad</HeaderTitle>
+          <DashboardArea>
 
-            <AreaSelect>
-              <HeaderSelectDay
-                selectedValue={selectDay}
-                useNativeAndroidPickerStyle={false}
-                style={{ height: 27, width: 150}}
-                Icon={() => (
-                  <IconContainer>
-                    <Entypo
-                      name="chevron-down"
-                      color={colors.PRIMARY_BLUE}
-                      size={20}
-                    />
-                  </IconContainer>
-                )}
-                onValueChange={(itemValue, itemIndex) => setSelectDay(itemValue)}
-              >
-                  <HeaderSelectDay.Item label="Today" value="1"/>
-              </HeaderSelectDay>
-            </AreaSelect>
+            <HeaderDashboard>
+              <HeaderTitle>Dashboad</HeaderTitle>
 
-         </HeaderDashboard>
-         
+              <AreaSelect>
+                <Picker 
+                  selectedValue={selectDay}
+                  style={{height:25, width:150, color:"#FFFFFF", backgroundColor:'transparent'}}
+                  dropdownIcon={false}
+                  onValueChange={(itemValue, itemIndex) => setSelectDay(itemValue)}
+                >
+                  <Picker.Item label="Today" value="1" />
+                  <Picker.Item label="A day ago" value="2" />
+                  <Picker.Item label="Two days ago" value="3" />
+                  <Picker.Item label="Three days ago" value="4" />
+                  <Picker.Item label="Four days ago" value="5" />
+                  <Picker.Item label="Five days ago" value="6" />
+                </Picker>
+                  <AreaIcon>
+                    <IconDrop width="20" height="20" fill="#FFF"  />
+                  </AreaIcon>
+              </AreaSelect>
+
+          </HeaderDashboard>
+
+          <AreaDays>
+              <Scroller
+                horizontal={true}
+                showsHorizontalScrollIndicator={false} 
+                style={{marginHorizontal:15}} >
+
+                <ScrollerItens>
+
+                  <ScrollItem  style={{marginLeft:0}}>
+                    <Day>1</Day>
+                    <Month>July</Month>  
+                  </ScrollItem>
+
+                  <ScrollItem>
+                    <Day>2</Day>
+                    <Month>July</Month>  
+                  </ScrollItem> 
+
+                  <ScrollItem>
+                    <Day>3</Day>
+                    <Month>July</Month>  
+                  </ScrollItem> 
+
+                  <ScrollItem>
+                    <Day  style={{fontSize:25, color:'#A7DCFE'}}>4</Day>
+                    <Month  style={{fontSize:16, fontWeight:'bold', color:'#A7DCFE'}}>July</Month>  
+                  </ScrollItem> 
+
+                  <ScrollItem>
+                    <Day>5</Day>
+                    <Month>July</Month>  
+                  </ScrollItem> 
+
+                  <ScrollItem>
+                    <Day>6</Day>
+                    <Month>July</Month>  
+                  </ScrollItem> 
+
+                  <ScrollItem>
+                    <Day>7</Day>
+                    <Month>July</Month>  
+                  </ScrollItem> 
+
+                  <ScrollItem>
+                    <Day>8</Day>
+                    <Month>July</Month>  
+                  </ScrollItem> 
+
+                </ScrollerItens>
+
+              </Scroller>
+            </AreaDays>
+
+            <LoginsAndErrorsArea>
+                    <Logins>
+                        <Title>Logins</Title>
+                        <Total>50</Total>
+                    </Logins>
+                    <Errors>
+                        <Title>Errors</Title>
+                        <Total>7100</Total>
+                    </Errors>
+            </LoginsAndErrorsArea>
+
+            <VisitorsArea>
+
+               <Title style={{marginBottom:15}}>Top visits</Title>
+
+               <ListVisits>
+                  <IpVisits>192.168.0.1</IpVisits>
+                  <TotalVisits>250.242 visits</TotalVisits>
+               </ListVisits>
+
+               <ListVisits>
+                  <IpVisits>192.168.0.1</IpVisits>
+                  <TotalVisits>20.467 visits</TotalVisits>
+               </ListVisits>
+
+               <ListVisits>
+                  <IpVisits>192.168.0.1</IpVisits>
+                  <TotalVisits>10.451 visits</TotalVisits>
+               </ListVisits>
+
+               <ListVisits>
+                  <IpVisits>192.168.0.1</IpVisits>
+                  <TotalVisits>345.125 visits</TotalVisits>
+               </ListVisits>
+
+            </VisitorsArea>
+
+          </DashboardArea>
          <Ads />
   </Container>
 )
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -6,26 +6,123 @@ export const Container = styled.SafeAreaView`
   background-color: #13162A;
 `;
 
+export const DashboardArea = styled.View`
+  padding:20px;
+`;
+
 export const HeaderDashboard = styled.View`
   flex-direction:row;
   justify-content: space-between;
-  margin:70px 0 20px 20px;
+  margin:60px 0 20px 20px;
 `;
 
 export const AreaSelect = styled.SafeAreaView`
   background-color:#2b2d40;
+  align-items:center;
+  flex-direction:row;
   border-radius:10px;
   margin-right:20px;
   width:170px;
 `;
 
-export const HeaderSelectDay = styled.Picker`
-  color:#FFF;
-  margin:0 auto;
+export const AreaIcon = styled.View`
+  margin-right:20px;
 `;
+
+export const AreaDays = styled.View`
+  width:100%;
+  margin:5px 0 0 0;
+  height:60px;
+`;
+
+export const ScrollerItens = styled.View`
+   flex-direction:row;
+   align-items:center;
+`;
+
+export const LoginsAndErrorsArea = styled.View`
+   flex-direction:row;
+   width:88%;
+   height:125px;
+   margin:20px 20px 0 20px;
+   justify-content:space-between;
+`;
+
+export const Logins = styled.View`
+  width:47%;
+  padding:20px;
+  height:125px;
+  background-color:#2b2d40;
+  border-radius:15px;
+`;
+
+export const Errors = styled.View`
+  width:47%;
+  height:125px;
+  background-color:#2b2d40;
+  border-radius:15px;
+  padding:20px;
+`;
+
+export const Title = styled.Text`
+  font-size:18px;
+  color:#FFF;
+`;
+
+export const Total = styled.Text`
+  font-size:24px;
+  color:#FFF;
+  margin-top:35px;
+  font-weight:bold;
+`;
+
+export const VisitorsArea = styled.View`
+   width:88%;
+   height:200px;
+   margin:20px;
+   padding:20px;
+   border-radius:15px;
+   background-color:#2b2d40;
+`;
+
+export const ListVisits = styled.View`
+   flex-direction:row;
+   justify-content:space-between;
+   margin-bottom:10px;
+`;
+
+export const IpVisits = styled.Text`
+  font-size:16px;
+  color:#FFF;
+  font-weight:bold;
+`;
+
+export const TotalVisits = styled.Text`
+  font-size:15px;
+  color:#FFF;
+`;
+
+export const Scroller = styled.ScrollView``;
 
 export const HeaderTitle = styled.Text`
   font-size:22px;
   color:#FFF;
   font-weight:bold;
+`;
+
+export const ScrollItem = styled.TouchableOpacity`
+  flex-direction:column;
+  align-items:center;
+  margin-left:25px;
+`;
+
+export const Day = styled.Text`
+  font-size:18px;
+  font-weight:bold;
+  color:#FFF;
+`;
+
+export const Month = styled.Text`
+  font-size:14px;
+  color:#403d3d;
 `;
